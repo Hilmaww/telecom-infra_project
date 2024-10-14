57bd8ca808d4b8b446b0c003fb69a5eeb76a5ce4
hilmaww <66060188+Hilmaww@users.noreply.github.com>
2024-10-15 04:53:44 +0700

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,25 +1,53 @@
-resource "aws_s3_bucket_versioning" "telecom_backup" {
-  bucket = "telecom-backup-bucket"
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
-  tags = {
-    Name = "telecom-backup"
-  }
+resource "aws_iam_role" "replication_role" {
+  name = "telecom-replication-role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "s3.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  })
 }
 
-resource "aws_s3_bucket_replication_configuration" "replication" {
-  bucket = aws_s3_bucket.telecom_backup.id
+resource "aws_iam_policy" "replication_policy" {
+  name = "telecom-replication-policy"
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:GetReplicationConfiguration",
+          "s3:ListBucket"
+        ],
+        "Resource": [
+          "arn:aws:s3:::telecom-backup-bucket"
+        ]
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:GetObjectVersion",
+          "s3:ReplicateObject",
+          "s3:ReplicateDelete",
+          "s3:ReplicateTags"
+        ],
+        "Resource": [
+          "arn:aws:s3:::telecom-backup-bucket/*"
+        ]
+      }
+    ]
+  })
+}
 
-  role = aws_iam_role.replication_role.arn
-
-  rules {
-    id     = "replication_rule"
-    status = "Enabled"
-    destination {
-      bucket        = aws_s3_bucket_telecom_backup_2.arn
-      storage_class = "STANDARD"
-    }
-  }
+resource "aws_iam_role_policy_attachment" "replication_role_attachment" {
+  role       = aws_iam_role.replication_role.name
+  policy_arn = aws_iam_policy.replication_policy.arn
 }
