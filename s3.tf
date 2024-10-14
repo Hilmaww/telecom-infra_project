c532423702b69bdc64bd41dc7d7c72071ab5eb36
hilmaww <66060188+Hilmaww@users.noreply.github.com>
2024-10-15 05:13:34 +0700

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,18 +1,39 @@
-resource "aws_s3_bucket_versioning" "telecom_backup" {
+# S3 Bucket resource
+resource "aws_s3_bucket" "telecom_backup" {
   bucket = "telecom-backup-bucket"
   acl    = "private"
+
   tags = {
     Name = "telecom-backup"
   }
 }
 
+# S3 Versioning resource
 resource "aws_s3_bucket_versioning" "telecom_backup_versioning" {
-  bucket = aws_s3_bucket.telecom_backup.bucket
+  bucket = aws_s3_bucket.telecom_backup.id
+  
   versioning_configuration {
     status = "Enabled"
   }
 }
 
+# S3 Replication Configuration (if replication is desired to another bucket)
+resource "aws_s3_bucket_replication_configuration" "replication" {
+  bucket = aws_s3_bucket.telecom_backup.id
+
+  role = aws_iam_role.replication_role.arn
+
+  rule {
+    id     = "replication_rule"
+    status = "Enabled"
+    destination {
+      bucket        = "arn:aws:s3:::destination-bucket-name"  # Replace with actual destination bucket ARN
+      storage_class = "STANDARD"
+    }
+  }
+}
+
+# IAM Role for S3 Replication
 resource "aws_iam_role" "replication_role" {
   name = "telecom-replication-role"
 
@@ -31,6 +52,7 @@ resource "aws_iam_role" "replication_role" {
   })
 }
 
+# IAM Policy for S3 Replication
 resource "aws_iam_policy" "replication_policy" {
   name = "telecom-replication-policy"
   policy = jsonencode({
@@ -62,6 +84,7 @@ resource "aws_iam_policy" "replication_policy" {
   })
 }
 
+# Attach IAM Policy to IAM Role for Replication
 resource "aws_iam_role_policy_attachment" "replication_role_attachment" {
   role       = aws_iam_role.replication_role.name
   policy_arn = aws_iam_policy.replication_policy.arn
