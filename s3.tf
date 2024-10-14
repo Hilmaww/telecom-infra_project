4b75b45ba904f99d13c1a984afec0c7edd19e883
hilmaww <66060188+Hilmaww@users.noreply.github.com>
2024-10-15 05:10:36 +0700

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,26 +1,15 @@
 resource "aws_s3_bucket_versioning" "telecom_backup" {
   bucket = "telecom-backup-bucket"
   acl    = "private"
-  versioning {
-	enabled = true
-  }
   tags = {
-	Name = "telecom-backup"
+    Name = "telecom-backup"
   }
 }
 
-resource "aws_s3_bucket_replication_configuration" "replication" {
-  bucket = aws_s3_bucket.telecom_backup.id
-
-  role = aws_iam_role.replication_role.arn
-
-  rule {
-	id     = "replication_rule"
-	status = "Enabled"
-	destination {
-	  bucket        = aws_s3_bucket_telecom_backup_2.arn
-	  storage_class = "STANDARD"
-	}
+resource "aws_s3_bucket_versioning" "telecom_backup_versioning" {
+  bucket = aws_s3_bucket.telecom_backup.bucket
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
